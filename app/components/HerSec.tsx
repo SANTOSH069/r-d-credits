c5498fbd75b4e266e1d36b53df2f94f0ed161394
Santosh <gvsantosh.05@gmail.com>
2026-02-28 23:42:53 +0530

    Initial R&D Push

---
+++ app/components/HerSec.tsx
@@ -0,0 +1,28 @@
+import { Button } from '@/components/ui/button'
+
+const HerSec = () => {
+  return (
+    <div className='flex flex-col items-center align-middle text-center justify-center mx-auto py-36'>
+        <div className='flex items-center flex-col lg:font-6xl text-black max-w-4xl mx-auto'>
+            <div className='scroll-m-20 text-center text-4xl font-bold tracking-tight text-balance'>
+                get maximum credits with
+            </div>
+            <div className='scroll-m-20 text-center text-4xl font-bold tracking-tight text-balance'>
+                minimum  <span className=' font-mono tracking-tighter text-blue-700'>efforts</span>
+            </div>
+        </div>
+        <p className='mt-2 max-w-xl text-center text-black/50 mx-auto'>
+           Cyrx is the only R&D tax credit solution that combines technical and R&D tax expertise with AI to simplify claim preparation and maximize returns.
+        </p>
+        <div className='flex flex-row items-center gap-4 mx-auto mt-4'>
+           <Button variant={`default`} value={`Get Started`} className=' hover:text-neutral-100 transition-all ease-in duration-150'>Get Started</Button>
+           <Button variant={`outline`} value={`Learn More`} className='hover:bg-blue-700 hover:text-neutral-100 transition-all ease-in duration-150'>Learn More</Button>
+        </div>
+        <div className='w-full h-auto mx-auto item-center mt-4'>
+          
+        </div>
+    </div>
+  ) 
+}
+
+export default HerSec
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex flex-row justify-between p-4'>
+      
+    </div>
+  )
+}
+
+export default Navbar
