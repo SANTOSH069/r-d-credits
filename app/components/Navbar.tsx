cc1ff7ef4efbe4a24c2315ae0f052d9c34a0c90e
Santosh <gvsantosh.05@gmail.com>
2026-03-01 14:11:12 +0530

    Pushed Navbar and Hero Section

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,34 @@
 import React from 'react'
-
+import DefLogo from '@/public/DefLogo.png'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
 const Navbar = () => {
   return (
-    <div className='flex flex-row justify-between p-4'>
-      
+    <div className='h-auto w-full  bg-white flex justify-between shadow-md p-2 top-0 '>
+      <div className='relative '>
+        <Image src={DefLogo} width={24} height={24} alt='Company Logo'/>
+      </div>
+      <div className='flex flex-row items-center gap-2 font-medium '>
+        <ul className='flex flex-row gap-4 '>
+          <Link href={'/'}>
+          <li className='text-neutral-300 hover:text-black ease-in transition-all duration-150'>
+            Home
+          </li>
+          </Link>
+          <Link href={'/docs'}>
+          <li className='text-neutral-300 hover:text-black ease-in transition-all duration-150'>
+            Docs
+          </li>
+          </Link>
+          <Link href={'/about-us'}>
+          <li className='text-neutral-300 hover:text-black ease-in transition-all duration-150'>
+            About
+          </li>
+          </Link>
+        </ul>
+      </div>
+      <Button variant={`default`}>Sign In</Button>
     </div>
   )
 }
---
+++ app/docs/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='min-h-screen w-full'>
+      <div>
+      </div>
+      <div className='mt-12'>
+
+      </div>
+    </div>
+  )
+}
+
+export default page
